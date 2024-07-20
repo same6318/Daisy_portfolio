37a823c425a79ace41ac5d2efa3d211103201a4c
same06 <typewreter@yahoo.co.jp>
2024-07-20 17:16:54 +0900

    管理者用企業一覧ページ作成中

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -1,7 +1,22 @@
 class Admin::CompaniesController < ApplicationController
 
   def index
-    companies = Company.all
+    @companies = Company
+    .select('companies.id, companies.name, COUNT(reviews.id) AS reviews_count')
+    .joins(users: :reviews)
+    .group('companies.id')
+    binding.irb
+  end
+
+  def show
+    @company = Company.find(params[:id])
+    @user = @company.user
+    @review_count = @company.users.joins(:reviews).count
+  end
+
+  def destroy
+    @company.destroy
+    redirect_to admin_companies_path
   end
 
 end
